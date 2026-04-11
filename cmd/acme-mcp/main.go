294f6902f4e26d43d5e7cdbf7ddd8aaf7e562b32
Fazlul Shahriar <fshahriar@gmail.com>
2026-04-11 13:49:54 -0400

    all: add MIT LICENSE

--- cmd/acme-mcp/main.go
+++ cmd/acme-mcp/main.go
@@ -1,3 +1,7 @@
+// Copyright © 2026 Fazlul Shahriar. All rights reserved.
+// Use of this source code is governed by the MIT License
+// that can be found in the LICENSE file.
+
 package main
 
 import (
